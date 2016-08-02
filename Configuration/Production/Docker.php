7e71b2f3ba2e98dea4ca1265242ccf17708a4ffc
Philipp Kitzberger <pk@cron.eu>
2016-08-02 08:49:05 +0000

    Added Production/Docker context

---
+++ Configuration/Production/Docker.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'DB' => array(
+        'database' => 'typo3',
+        'host'     => 'mysql',
+        'port'     => '3306',
+        'username' => 'dev',
+        'password' => 'dev',
+    ),
+);
