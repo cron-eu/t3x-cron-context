94bed93a08df0679cead80ffbfd28da4ead4f978
Nino Avsajanishvili <na@cron.eu>
2017-08-16 15:57:38 +0000

    Full TYPO3 8.7 compatibility

--- Configuration/Development/Docker.php
+++ Configuration/Development/Docker.php
@@ -16,5 +16,14 @@ return array(
         'port'     => $_ENV['MYSQL_PORT'] ?: '3306',
         'username' => $_ENV['MYSQL_USER'] ?: 'dev',
         'password' => $_ENV['MYSQL_PASS'] ?: 'dev',
+        'Connections' => array(
+            'Default' => array(
+                'dbname'   => $_ENV['MYSQL_DB']   ?: 'typo3',
+                'host'     => $_ENV['MYSQL_HOST'] ?: 'mysql',
+                'port'     => $_ENV['MYSQL_PORT'] ?: '3306',
+                'user'     => $_ENV['MYSQL_USER'] ?: 'dev',
+                'password' => $_ENV['MYSQL_PASS'] ?: 'dev',
+            ),
+        ),
     ),
 );
