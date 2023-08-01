4c9a9d7c2c71c0f11237761b8d4ed2349476b211
Ernesto Baschny <eb@cron.eu>
2023-08-01 17:14:30 +0200

    Consolidate */Docker setup in one file

--- Configuration/Production/Docker.php
+++ Configuration/Production/Docker.php
@@ -1,16 +1,3 @@
 <?php
 
-return [
-    'DB' => [
-        'Connections' => [
-            'Default' => [
-                'driver'   => 'mysqli',
-                'dbname'   => $_ENV['MYSQL_DB']   ?? 'typo3',
-                'host'     => $_ENV['MYSQL_HOST'] ?? 'mysql',
-                'port'     => $_ENV['MYSQL_PORT'] ?? '3306',
-                'user'     => $_ENV['MYSQL_USER'] ?? 'dev',
-                'password' => $_ENV['MYSQL_PASS'] ?? 'dev',
-            ],
-        ],
-    ],
-];
+return include __DIR__ . '/../Development/Docker.php';
