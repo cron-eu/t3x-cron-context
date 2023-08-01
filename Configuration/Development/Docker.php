d1ea604e110460445318c7e84b6522b60cc68252
Ernesto Baschny <eb@cron.eu>
2023-08-01 16:59:56 +0200

    Remove doNotCheckReferer (obsolete since v8)
    See https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/8.7.x/Important-83768-RemoveReferrerCheck.html

--- Configuration/Development/Docker.php
+++ Configuration/Development/Docker.php
@@ -6,8 +6,7 @@ return [
         'transport_smtp_server' => 'mail:1025'
     ],
     'SYS' => [
-        'trustedHostsPattern' => '.*',
-        'doNotCheckReferer'   => TRUE,
+        'trustedHostsPattern' => '.*'
     ],
     'GFX' => [
         'processor_path'      => '/usr/bin/',
